077dde25d743d9d995de8661f7ea98f32de53229
vitorboccio <vitorboccio@gmail.com>
2021-05-27 14:58:10 -0400

    card

---
+++ components/Card.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.css'
+
+const Card = ({ href, children }) => {
+  return (
+    <Link href={href}>
+      <a className={styles.card}>
+        {children}
+      </a>
+    </Link>
+  );
+}
+
+export default Card;
