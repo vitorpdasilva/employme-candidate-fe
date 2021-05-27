42d1c308c4859f75ddedccfd3535e86316e10178
vitorboccio <vitorboccio@gmail.com>
2021-05-27 17:46:53 -0400

    ok card

--- pages/index.js
+++ pages/index.js
@@ -19,15 +19,18 @@ export default function Home() {
         <div className={styles.grid}>
           <Card href="https://nextjs.org/docs">
             <h2>Software Engineer (Remote only) &rarr;</h2>
+            <ul style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <li>Vancouver - Canada</li>
+              <li>Relocation Only (Sponsored Visa)</li>
+              <li>$80,000 up to $100,000 CAD/Annual</li>
+            </ul>
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-              when an unknown printer took a galley of type and scrambled it to make a type 
-              specimen book. It has survived not only five centuries, but also the leap into 
-              electronic typesetting, remaining essentially unchanged. It was popularised in 
-              the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
-              and more recently with desktop publishing software like Aldus PageMaker including 
-              versions of Lorem Ipsum.
+              when an unknown printer took a galley
             </p>
+            <div>
+              <span>Javascript 3 yrs</span>
+            </div>
           </Card>
         </div>
       </main>
