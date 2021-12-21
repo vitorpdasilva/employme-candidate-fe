fa765972b98b165d7f9518143e1ee8c15eeda7fa
vitorboccio <vitorboccio@gmail.com>
2021-12-21 15:28:58 -0800

    signup

--- pages/signup.js
+++ pages/signup.js
@@ -1,24 +1,40 @@
 import { StyledFormHolder } from "./auth/style";
 import { useForm } from "react-hook-form";
+import { useRouter } from 'next/router'
 import { Button } from "../components";
-const body = {
-  username: `-test-0.8187198489084304@test.com`,
-  password: 'test123', //$2b$10$lWELf94CfOxyWsfe2e350eDOWFprt0F5Jscd8GXoQPWO7nK6sm1TG
-  name: `Test - ${Math.random()}`,
-}
+import { fetchApi } from "./client"; 
 
 const Register = () => {
-  const submit = async () => {
-    const res = await fetchApi({ url: '/register', body });
-    console.log({ res });
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const router = useRouter()
+  const submit = async ({ email, username, password }) => {
+    const body = {
+      email,
+      username,
+      password
+    }
+    try {
+      const res = await fetchApi({ url: '/register', body });
+      console.log({ res });
+      router.push('/jobs');
+
+    } catch (err) {
+      console.error(err);
+    }
   }
   return (
     <div style={{ width: 500 }}>
       <StyledFormHolder>
-        <form>
-          <input placeholder="username" />
-          <input placeholder="email" />
-          <input placeholder="password" />
+        <form onSubmit={handleSubmit(submit)}>
+          {errors.email && <small>This field is required</small>}
+          <input type="email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} placeholder="email" />
+          
+          {errors.username && <small>This field is required</small>}
+          <input {...register("username", { required: true })} placeholder="username" />
+          
+          {errors.password && <small>This field is required</small>}
+          <input type="password" {...register("password", { required: true })} placeholder="password" />
+          
           <Button>Register</Button>
         </form>
       </StyledFormHolder>
